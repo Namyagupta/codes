f45aa690fdb639a717f0f7d51ab8dc58b771bfb9
Namya <namyagupta2001@gmail.com>
2023-01-31 19:50:41 +0530

    sieve

---
+++ 1.Basic/prime_factor.cpp
@@ -0,0 +1,30 @@
+//
+#include<iostream>
+using namespace std;
+
+void primeSieve(int n){
+    int prime[500]= {0};
+
+    for(int i=2; i<=n; i++){
+        if (prime[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                prime[j]=1;
+            }
+        }
+    }
+
+    for(int i=2; i<=n; i++){
+        if(prime[i]==0){
+            cout<<i<<" ";
+        }
+    }cout<<endl;
+}
+void smallest(){
+    int h
+}
+int main(){
+    int n;
+    cin>>n; 
+    primeSieve(n);
+    return 0;
+}
