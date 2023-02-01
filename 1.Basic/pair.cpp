5552e3957b7a9e48059a617edf23793e14240e7e
Namya <namyagupta2001@gmail.com>
2023-02-01 19:49:00 +0530

    vect and pairs

--- 1.Basic/pair.cpp
+++ 1.Basic/pair.cpp
@@ -1,9 +1,35 @@
+//Goal- get the values of vector between (0 to the size of arr)-1 
+
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int m
+//comparator - custom - useful in sorting
+bool myCompare(pair<int,int> p1, pair <int,int> p2){
+    return p1.first<p2.first;
+}
 
 int main(){
-    int a;
+    // pair <int,char> p;
+    // p.first=3;
+    // p.second='a';
+    int arr[] ={10,16,7,14,5,3,2,9} ;
+    //vector of pairs
+    vector<pair <int,int> > v;
 
+    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        v.push_back(make_pair(arr[i],i));
+
+    }
+
+    sort(v.begin(),v.end(), myCompare);
+
+    for(int i=0; i<v.size(); i++){
+        arr[v[i].second] = i;
+    }
+
+    for(int i=0; i<v.size(); i++){
+        cout<<arr[i];
+    }
+    return 0;
 }
--- 1.Basic/vectors.cpp
+++ 1.Basic/vectors.cpp
@@ -1,24 +1,28 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+#include <numeric>
 
 int main(){
     vector<int> v;
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
+    v.push_back(5);
+    v.push_back(6);
+    v.push_back(9);
 
     //iteration throug vector ways
     //1. iterate through vector like arrays
-    for(int i=0; i<v.size(); i++){
-        cout<<v[i]<<endl;
-    }
+    // for(int i=0; i<v.size(); i++){
+    //     cout<<v[i]<<endl;
+    // }
 
     //2. iterate through vector with it pointer
-    vector<int>:: iterator it;
-    for(it=v.begin();it!=v.end(); it++){
-        cout<<*it<<endl;
-    }
+    // vector<int>:: iterator it;
+    // for(it=v.begin();it!=v.end(); it++){
+    //     cout<<*it<<endl;
+    // }
     
     //3. iterate through vector with auto keyword
     for(auto element: v){
@@ -44,6 +48,21 @@ int main(){
     
     //sorting the vectors
     sort(v.begin(), v.end());
-    
+
+    // To get the sum of the vector
+    // We use accumulate(), defined in the <numeric> library.
+    int sum = accumulate(v.begin(),v.end(),0);
+    cout<<sum<<endl;
+
+    //converts vector into prefix_sum vector
+    int prefix_su = *partial_sum(v.begin(), v.end(), v.begin());
+    cout<<prefix_su<<endl;
+
+    //max element
+    cout<<"Max element: "<<*max_element(v.begin(), v.end())<<endl;
+    cout<<"Min element: "<<*min_element(v.begin(), v.end())<<endl;
+
+    //2D Vector: 3X3  and declare it -1
+    vector<vector<int> > a(3, vector<int>(3,-1));
     return 0;
 }
