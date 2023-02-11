10a97442fd890e1a54d135d7055cc67ef63c4b40
Namya <namyagupta2001@gmail.com>
2023-02-11 12:54:10 +0530

    del

--- 7.linkedLists/doublyLl.cpp
+++ 7.linkedLists/doublyLl.cpp
@@ -3,24 +3,81 @@ using namespace std;
 
 class node{
     public:
+
     int data;
     node* next;
     node* prev;
 
     node(int val){
-        data = val;
-        next = NULL;
-        prev = NULL;
+        data=val;
+        next=NULL;
+        prev=NULL;
     }
 };
 
+void insertAtHead(node* &head, int val){
+
+    node* n= new node(val);
+    n->next=head;
+    if(head!=NULL){
+        head->prev=n;
+    }
+
+    head=n;
+}
+
+
 void insertAtTail(node* &head, int val){
-    node* temp = head; 
-    
+
+    if(head==NULL){
+        insertAtHead(head,val);
+        return;
+    }
+
+    node* n=new node(val);
+    node* temp=head;
+
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+
+    temp->next=n;
+    n->prev=temp;
+
+}
+
+void deleteNode(node* &head, int val){
+    node* temp = head;
+
+    //if todel is the first node
+    if(val == head->data){
+        node* toDel = head;
+        head = head->next;
+        head->prev = NULL;
+        delete toDel;
+        return;
+    }
+
+    //not the first node
+    while(temp->data!=val){
+        temp = temp->next;
+    }
+    temp->prev->next = temp->next;
+
+    //if to del is not the last node
+    if(temp->next!=NULL){
+        temp->next->prev = temp->prev;
+    }
+    delete temp;
 }
 
 void display(node* head){
-    node* temp = head;
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
 }
 
 int main(){
@@ -31,4 +88,9 @@ int main(){
     insertAtTail(head,4);
     insertAtTail(head,5);
     display(head);
+    insertAtHead(head,9);
+    display(head);
+    deleteNode(head, 4);
+    display(head);
+    return 0;
 }
