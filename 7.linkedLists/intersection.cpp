14aae76f77a6abbbc79f80b5309250526e2a82f5
Namya <namyagupta2001@gmail.com>
2023-02-12 13:19:24 +0530

    intersection

--- 7.linkedLists/intersection.cpp
+++ 7.linkedLists/intersection.cpp
@@ -1,3 +1,5 @@
+
+
 #include<iostream>
 using namespace std;
 
@@ -11,6 +13,12 @@ class node{
         next=NULL;
     }
 };
+
+void insertAthead(node* &head, int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
 
 void insertAtTail(node* &head, int val){
 
@@ -29,6 +37,22 @@ void insertAtTail(node* &head, int val){
 
 }
 
+void intersect(node* &head1, node* &head2, int pos){
+    
+    node* temp1=head1;
+    pos--;
+    while(pos--){
+        temp1=temp1->next;
+    }
+    node* temp2=head2;
+    while(temp2->next!=NULL){
+        temp2=temp2->next;
+    }
+
+    temp2->next=temp1;
+
+}
+
 void display(node* head){
     node* temp=head;
     while(temp!=NULL){
@@ -38,16 +62,76 @@ void display(node* head){
     cout<<"NULL"<<endl;
 }
 
-int main(){
-    node* head=NULL;
-    int arr[]={1,2,3,4,5,6};
-    for(int i=0;i<6;i++){
-        insertAtTail(head,arr[i]);
+int length(node* head){
+    int l=0;
+    node* temp=head;
+
+    while(temp!=NULL){
+        temp=temp->next;
+        l++;
     }
-    display(head);
-    int k;
-    cout<<"Pls enter k: ";
-    cin>>k;
-    node* newHead = appendNodes(head,k);
-    display(newHead);
+    return l;
+}
+
+int intersection(node* &head1, node* &head2){
+
+    int l1=length(head1);
+    int l2=length(head2);
+
+    int d=0;
+    node* ptr1;
+    node* ptr2;
+    //assign ptr1 to larger linked list 
+    //ptr2 to smaller linked list 
+    //d will give the difference between the two linked lists so we will start from there
+    if(l1>l2){
+        d=l1-l2;
+        ptr1=head1;
+        ptr2=head2;
+    }
+    else{
+        d=l2-l1;
+        ptr1=head2;
+        ptr2=head1;
+    }
+    
+    //traversing ptr 1 to the difference between the two lists for the larger linked list 
+    while(d){
+        ptr1=ptr1->next;
+        if(ptr1==NULL){
+            return -1;
+        }
+        d--;
+    }
+
+    while(ptr1!=NULL && ptr2!=NULL){
+
+        if(ptr1==ptr2){
+            return ptr1->data;
+        }
+        ptr1=ptr1->next;
+        ptr2=ptr2->next;
+    }
+
+    return -1;
+
+}
+
+int main(){
+    node* head1=NULL;
+    node* head2=NULL;
+    insertAtTail(head1,1);
+    insertAtTail(head1,2);
+    insertAtTail(head1,3);
+    insertAtTail(head1,4);
+    insertAtTail(head1,5);
+    insertAtTail(head1,6);
+    insertAtTail(head2,9);
+    insertAtTail(head2,10);
+    intersect(head1,head2,3);
+    display(head1);
+    display(head2);
+    cout<<intersection(head1,head2)<<endl;
+
+    return 0;
 }
