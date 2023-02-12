b1e65f56f6beda6b556e64c83a4abe4405295341
Namya <namyagupta2001@gmail.com>
2023-02-12 13:52:41 +0530

    merge rec

--- 7.linkedLists/merge.cpp
+++ 7.linkedLists/merge.cpp
@@ -64,6 +64,28 @@ node* merge(node* &head1, node* &head2){
     return head3->next;
 }
 
+// node* mergeRecursive(node* &head1, node* &head2){
+
+//     if(head1==NULL){
+//         return head2;
+//     }
+//     if(head2==NULL){
+//         return head1;
+//     }
+
+//     node* result;
+//     if(head1->data<head2->data){
+//         result=head1;
+//         result->next=mergeRecursive(head1->next,head2);
+//     }
+//     else{
+//         result= head2;
+//         result->next=mergeRecursive(head1,head2->next);
+//     }
+
+//     return result;
+// }
+
 int main(){
     node* head1=NULL;
     node* head2=NULL;
