44100b5f8142a0450eda16c553917ba9f8468a54
Namya <namyagupta2001@gmail.com>
2023-02-16 18:38:27 +0530

    stacks

---
+++ 8.stacks/arrStack.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void push(int x){
+    int 
+}
+int main(){
+    int arr[1000];
+    push(2);
+    push(4);
+    push(6);
+    push(7);
+    push(8);
+    top();
+    pop();
+    top();
+    empty();
+    return 0;
+}
